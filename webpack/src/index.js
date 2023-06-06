276a29e2cf750a4be559fd95acceb25c6abf4f3a
yubiyun <245817300@qq.com>
2023-06-06 15:16:42 +0800

    update

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,22 @@
-const a='hi'
-console.log(a)
+// import "./style.css";
+// import png from "./images/1.png";
+
+// const h1 = document.createElement("h1");
+// h1.innerHTML = "hello world";
+// document.body.appendChild(h1);
+
+// const img = document.createElement("img");
+// img.src = png;
+// document.body.appendChild(img);
+
+const button = document.createElement("button");
+button.innerText = "懒加载";
+button.addEventListener("click", () => {
+  import("./a.js").then(
+    (a) => {
+      console.log(a);
+    },
+    () => {}
+  );
+});
+document.body.appendChild(button)
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,25 @@
-const path = require("path");
+// const path = require("path");
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ],
   },
+  // entry: "./src/index.js",
+  // output: {
+  //   path: path.resolve(__dirname, "dist"),
+  //   filename: "bundle.js",
+  // },
 };
